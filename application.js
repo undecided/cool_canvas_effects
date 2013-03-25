5b0ce648d4d3f562dcf0c00e5dbbb5ca252970fb
Matthew Bennett <matthew@quickwebdesign.net>
2013-03-25 18:12:23 +0000

    Got a canvas

--- application.js
+++ application.js
@@ -1,3 +1,24 @@
 window.onload = function() {
-  alert('Hello, great world');
+  var canvas = document.createElement('canvas');
+  var context = canvas.getContext('2d');
+  document.body.appendChild(canvas);
+
+
+
+  var repaint = function() {
+    context.fillStyle = "rgba(0, 255, 0, 0.5)";
+    context.fillRect(0,0,canvas.width, canvas.height);
+  };
+
+
+
+  var resize = function() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    repaint();
+  };
+
+  window.onresize = resize;
+  resize();
+
 };
