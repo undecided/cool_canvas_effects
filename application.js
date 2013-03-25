5a4dc0e4138d529aa896fd7ce188a7c1b2c83ae6
Matthew Bennett <matthew@quickwebdesign.net>
2013-03-25 18:24:00 +0000

    Random squares

--- application.js
+++ application.js
@@ -6,10 +6,18 @@ window.onload = function() {
 
 
   var repaint = function() {
-    context.fillStyle = "rgba(0, 255, 0, 0.5)";
+    context.fillStyle = "rgba(0, 255, 0, 0.1)";
     context.fillRect(0,0,canvas.width, canvas.height);
-  };
 
+    var random_size = Math.round(Math.random() * 30);
+    var random_x = Math.round(Math.random() * canvas.width);
+    var random_y = Math.round(Math.random() * canvas.height);
+
+    context.fillStyle = "rgb(0, 128, 255)";
+    context.fillRect(random_x, random_y, random_size, random_size);
+
+    setTimeout(repaint, 100);
+  };
 
 
   var resize = function() {
@@ -17,6 +25,7 @@ window.onload = function() {
     canvas.height = window.innerHeight;
     repaint();
   };
+
 
   window.onresize = resize;
   resize();
